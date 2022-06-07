b967c7e987231e4dd8371e09454f8058b4546c6c
Djordje Jovanovic <djordjejovanovic27@gmail.com>
2022-06-07 14:27:01 +0200

    Pretraga svih kretanja vozova na stranici za kupovinu karata

--- Tim14HCI/Tim14HCI/Windows/ClientWindow.xaml.cs
+++ Tim14HCI/Tim14HCI/Windows/ClientWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Tim14HCI.Contorls;
+using Tim14HCI.DAO;
 using Tim14HCI.Model;
 
 namespace Tim14HCI.Windows
@@ -23,6 +25,14 @@ namespace Tim14HCI.Windows
     {
         Window parent;
         User user;
+        int chosenDeparture;
+
+        String startLocationSearch;
+        String endLocationSearch;
+        String startDatetimeSearch;
+        String endDatetimeSearch;
+
+        String[] acceptableDateTimeFormats = { "dd.MM.yyyy.", "dd.MM.yyyy. HH:mm", "dd.MM.yyyy. HH:mm:ss" };
 
         public ClientWindow()
         {
@@ -47,8 +57,10 @@ namespace Tim14HCI.Windows
         private void DepartureChosenEventHandler(object sender, RoutedEventArgs e)
         {
             DepartureChosenEventArgs args = (DepartureChosenEventArgs)e;
-            Console.WriteLine(args.DepartureID);
-            //btn_synchronize.IsEnabled = true;
+            chosenDeparture = args.DepartureID;
+            Departure departure = DepartureDAO.GetDepartureByID(chosenDeparture);
+            lbl_departure.Content = departure.startDate.ToString("dd.MM.yyyy. HH:mm") + "  " + departure.endDate.ToString("dd.MM.yyyy. HH:mm") + "  " + departure.TrainLine.StartStation.Name + "  Novi Sad";
+
         }
 
 
@@ -60,7 +72,6 @@ namespace Tim14HCI.Windows
 
             foreach (Departure departure in departures)
             {
-
                 DepartureControl departureControl = new DepartureControl(departure);
                 stack_Data.Children.Add(departureControl);
             }
@@ -75,6 +86,159 @@ namespace Tim14HCI.Windows
         {
             parent.Show();
             Close();
+        }
+
+        private void ReservationClick(object sender, RoutedEventArgs e)
+        {
+            Ticket ticket = new Ticket();
+            ticket.User = user;
+            ticket.ForReservation = true;
+            ticket.Departure = DepartureDAO.GetDepartureByID(chosenDeparture);
+            TicketDAO.AddNewTicket(ticket);
+            MessageBox.Show("Karta uspešno rezervisana!");
+        }
+
+        private void BuyClick(object sender, RoutedEventArgs e)
+        {
+            Ticket ticket = new Ticket();
+            ticket.User = user;
+            ticket.ForReservation = false;
+            ticket.Departure = DepartureDAO.GetDepartureByID(chosenDeparture);
+            TicketDAO.AddNewTicket(ticket);
+            MessageBox.Show("Karta uspešno kupljena!");
+        }
+
+        private void ShowBoughtTickets(object sender, RoutedEventArgs e)
+        {
+            TicketDisplayWindow ticketDisplayWindow = new TicketDisplayWindow(this, "Bought", user);
+            Visibility = Visibility.Hidden;
+            ticketDisplayWindow.Show();
+        }
+
+        private void ShowReservationTickets(object sender, RoutedEventArgs e)
+        {
+            TicketDisplayWindow ticketDisplayWindow = new TicketDisplayWindow(this, "Reservation", user);
+            Visibility = Visibility.Hidden;
+            ticketDisplayWindow.Show();
+        }
+
+        private void Search(object sender, RoutedEventArgs e)
+        {
+            List<Departure> departures = DAO.DepartureDAO.getDapertures();
+
+            this.startLocationSearch = txbx_start_location.Text;
+            this.endLocationSearch = txbx_end_location.Text;
+            this.startDatetimeSearch = txbx_start_time.Text;
+            this.endDatetimeSearch = txbx_end_time.Text;
+
+            if (!CheckDatetimeInput())
+            {
+                MessageBox.Show("Pogrešan format datuma!\nPrimeri prihvatljivih formata datuma: 21.04.2022. ili 21.04.2022. 14:00");
+                return;
+            }
+
+            List<Departure> startingStationFiltered = FilterByStartingStation(departures);
+            List<Departure> destinationFiltered = FilterByDestination(startingStationFiltered);
+            List<Departure> startDateFiltered = FilterByStartDatetime(destinationFiltered);
+            List<Departure> endDateFiltered = FilterByEndDatetime(startDateFiltered);
+
+            stack_Data.Children.Clear();
+            foreach (Departure departure in endDateFiltered)
+            {
+                DepartureControl departureControl = new DepartureControl(departure);
+                stack_Data.Children.Add(departureControl);
+            }
+
+        }
+
+        private List<Departure> FilterByEndDatetime(List<Departure> departures)
+        {
+            List<Departure> filtered = new List<Departure>();
+            if (this.endDatetimeSearch != "")
+            {
+                DateTime dt = DateTime.ParseExact(this.endDatetimeSearch, "dd.MM.yyyy. HH:mm", System.Globalization.CultureInfo.InvariantCulture);
+                foreach (Departure departure in departures)
+                {
+                    if (DateTime.Compare(departure.endDate, dt) <= 0)
+                    {
+                        filtered.Add(departure);
+                    }
+                }
+            }
+            else
+            {
+                filtered = departures;
+            }
+            return filtered;
+        }
+
+        private List<Departure> FilterByStartDatetime(List<Departure> departures)
+        {
+            List<Departure> filtered = new List<Departure>();
+            if (this.startDatetimeSearch != "")
+            {
+                DateTime dt = DateTime.ParseExact(this.startDatetimeSearch, "dd.MM.yyyy. HH:mm", System.Globalization.CultureInfo.InvariantCulture);
+                foreach (Departure departure in departures)
+                {
+                    if (DateTime.Compare(departure.startDate, dt) >= 0)
+                    {
+                        filtered.Add(departure);
+                    }
+                }
+            }
+            else
+            {
+                filtered = departures;
+            }
+            return filtered;
+        }
+
+        private List<Departure> FilterByDestination(List<Departure> departures)
+        {
+            return departures;
+        }
+
+        private List<Departure> FilterByStartingStation(List<Departure> departures)
+        {
+            List<Departure> filtered = new List<Departure>();
+            if (this.startLocationSearch != "")
+            {
+                foreach(Departure departure in departures)
+                {
+                    if (departure.TrainLine.StartStation.Name.ToLower() == this.startLocationSearch.ToLower())
+                    {
+                        filtered.Add(departure);
+                    }
+                }
+            }
+            else
+            {
+                filtered = departures;
+            }
+            return filtered;
+        }
+
+        private bool CheckDatetimeInput()
+        {
+            DateTime dt;
+            
+            if (this.startDatetimeSearch != "")
+            {
+                if (!DateTime.TryParseExact(this.startDatetimeSearch, this.acceptableDateTimeFormats, CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out dt))
+                {   
+                    return false;
+                }
+            }
+
+            if (this.endDatetimeSearch != "")
+            {
+                if (!DateTime.TryParseExact(this.endDatetimeSearch, this.acceptableDateTimeFormats, CultureInfo.InvariantCulture, DateTimeStyles.None, out dt))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }

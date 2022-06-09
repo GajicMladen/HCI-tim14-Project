4473dc7f5f519bc18913de4a9b2cca6b037899fe
Jovan Tomic <jtomic1@gmail.com>
2022-06-09 13:41:20 +0200

    Departure

---
+++ Tim14HCI/Tim14HCI/Contorls/DepartureControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Tim14HCI.Model;
+
+namespace Tim14HCI.Contorls
+{
+    /// <summary>
+    /// Interaction logic for DepartureControl.xaml
+    /// </summary>
+    public partial class DepartureControl : UserControl
+    {
+        Departure departure;
+
+        public DepartureControl()
+        {
+            InitializeComponent();
+        }
+
+        public DepartureControl(Departure x)
+        {
+            departure = x;
+            InitializeComponent();
+
+            lbl_Line.Content = departure.TrainLine.StartStation.Name + " - " + departure.TrainLine.EndStation.Station.Name;
+        }
+    }
+}
